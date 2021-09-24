f61c6abc5c98a7c64ef26fce1d523440c752aa38
root <root@eastern-racing.tr130.co.uk>
2021-09-24 16:06:36 +0000

    added contact email functionality

--- contact.php
+++ contact.php
@@ -1,3 +1,14 @@
 <?php
 
-echo 'hello';
+$message = "";
+if ($_POST) {
+    $message = "Name: " . $_POST['name'] . "\r\n";
+    $message .= "Phone: " . $_POST['phone'] . "\r\n";
+    $message .= "Email: " . $_POST['email'] . "\r\n";
+    $message .= "Subject: " . $_POST['subject'] . "\r\n";
+    $message .= "Message: " . $_POST['message'] . "\r\n";
+    $message = wordwrap($message, 70, "\r\n");
+}
+mail('henry@tr130.co.uk', 'contact form', $message);
+
+header('Location: https://tr130.co.uk#contact');
